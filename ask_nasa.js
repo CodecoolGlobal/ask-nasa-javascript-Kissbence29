713b2a9b028147a7bdcef78aa57cd851334bedf5
Kissbence29 <kissbence19@gmail.com>
2022-07-27 16:51:59 +0200

    refactor:deleteing decideUrl function

--- ask_nasa.js
+++ ask_nasa.js
@@ -1,26 +1,19 @@
 function main(){
 
 const baseDiv = document.querySelector("#root");
-let today = new Date().toISOString().slice(0, 10)
+const inputDate = document.querySelector("#date");
+let today = new Date().toISOString().slice(0, 10);
+inputDate.value = today;
 const baseUrl = "https://api.nasa.gov/planetary/apod?api_key=1Y2xps6PFrqfili1DbLJ26MQOXxHk3qiOZJvhCe9&date=";
 const submit = document.querySelector("#submit")
 submit.addEventListener("click",(e)=>{
     e.preventDefault();
-    const date= document.querySelector("#date").value;
-    createPage(decideUrl(date));
-    
+    const date= inputDate.value;
+    createPage(baseUrl+date);
 })
 
-function decideUrl(date){
-    if(date===""){
-        return baseUrl;
-    }
-    else{
-        return baseUrl+ date
-    }
-}
-
 async function ApiRequest(baseUrl){
+    console.log(baseUrl);
     const response = await fetch(baseUrl);
     const data = await response.json();
     return data;
@@ -37,18 +30,15 @@ function decideBetweenPictureAndVideo(data){
 }
 
 async function createPage(baseUrl){
+    console.log(date);
     const data = await ApiRequest(baseUrl);
     let source= decideBetweenPictureAndVideo(data);
     const explanation = data.explanation ;
     const title= data.title;
-    
-    
     baseDiv.innerHTML=`<h1 style="text-align:center">${title}</h1><p>${explanation}</p><p>${source}</p>`
+}
 
-
- }
 createPage(baseUrl+today);
-
 }
 
 main();
